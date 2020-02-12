4f8597c827a51722d42cda9b984e76a5ddcc1581
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-02-11 19:59:05 -0500

    reformatted routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,112 @@
+// Require Packages
+const axios = require("axios");
+const cheerio = require("cheerio");
+const app = require("express");
+
+// Require Models
+const db = require("./../models")
+// const Article = require("./../models/Article.js")
+// const Note = require("./../models/Note.js")
+
+module.exports = function(app) {
+
+// Scrape Route
+app.get("/api/scrape", function(req, res) {
+    axios.get("https://nintendoeverything.com/").then(function(res){
+        const $ = cheerio.load(res.data);
+
+        // Save desired info from scrape
+        $(".row.text-left").each(function(index, value) {
+            const title = $(this).find("h2").text().trim();
+            const url = $(this).find("a").attr("href");
+            const desc = $(this).find(".description").children("div.large-12").eq(1).children("p").eq(1).text();
+            const image = $(this).find("img.attachment-large").attr("src");
+            
+            // Create object from variables
+            let result = {
+                "title": title,
+                "url": url,
+                "desc": desc,
+                "image": image
+            };
+
+            // Create Article from schema
+            db.Article.create(result)
+                .then(function(dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    console.log(err.message)
+                });
+        });
+    })
+    .then(res.redirect("./htmlRoutes/all"));
+});
+
+
+app.get("/api/articles", function(req, res) {
+    db.Article.find({})
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err)
+        });
+});
+
+app.put("/api/articles/:id", function(req, res) {
+    let saved;
+    if (req.body.saved) {
+        saved = false;
+    } else {
+        saved = true;
+    }
+
+    db.Article.update({ _id: req.params.id}, {$set: { saved: saved } })
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+app.get("/api/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+app.post("/api/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+        .then(function(dbNote) {
+            return db.Article.findOneAndUpdate(
+                { _id: req.params.id },
+                { note: dbNote._id },
+                { new: true }
+            );
+        })
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+app.get("/api/saved", function(req, res) {
+    db.Article.find({ saved: true })
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+};
--- server.js
+++ server.js
@@ -2,13 +2,11 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const axios = require("axios");
-const cheerio = require("cheerio");
-const handlebars = require("express-handlebars")
+const exphbs = require("express-handlebars")
 
 // Require Models
-const Article = require("./models/Article.js")
-const Note = require("./models/Note.js")
+const Article = require("./models/Article.js");
+const Note = require("./models/Note.js");
 
 const PORT = 3000;
 
@@ -21,85 +19,66 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Setting up Handlebars
-app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Setting up connection to Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
 mongoose.connect(MONGODB_URI);
 
-// Routes
-app.get("/", function(req, res) {
-    // If there's nothing in collection, scrape
-    if (!MONGODB_URI.articles) {
-        res.redirect("/scrape")
-        return;
-    // Else, show all
-    } else {
-        res.redirect("/all")
-    }
-});
+// Requiring Routes
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
 
-// Scrape Route
-app.get("/scrape", function(req, res) {
-    axios.get("https://nintendoeverything.com/").then(function(res){
-        const $ = cheerio.load(res.data);
-
-        // Save desired info from scrape
-        $(".row.text-left").each(function(index, value) {
-            const title = $(this).find("h2").text().trim();
-            const url = $(this).find("a").attr("href");
-            const desc = $(this).find(".description").children("div.large-12").eq(1).children("p").eq(1).text();
-            const image = $(this).find("img.attachment-large").attr("src");
-            
-            // Create object from variables
-            let result = {
-                "title": title,
-                "url": url,
-                "desc": desc,
-                "image": image
-            };
-
-            // Create Article from schema
-            Article.create(result)
-                .then(function(dbArticle) {
-                    console.log(dbArticle);
-                })
-                .catch(function(err) {
-                    console.log(err.message)
-                });
-        });
-    })
-    .then(res.redirect("/all"));
-});
-
-app.get("/all", function(req, res) {
-    Article.find({}).then(function(dbArticle) {
-    let articles = [];
-        for (const el of dbArticle) {
-            articles.push({ id: el._id, title: el.title, desc: el.desc, url: el.url, image: el.image });
-        };
-        res.render("index", { article: articles });
-    });
-});
-
-app.get("/notes", function(req, res) {
-    Note.find({})
-        .then(function(dbNote){
-            res.json(dbNote);
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
-});
-
-    // res.render 
-    // note button
-        // include art id as attr
-
-// save notes /:id (post)
-    // link note and article id
-
+// Start server
 app.listen(PORT, function() {
     console.log(`Listening on port ${PORT}`)
 });
+
+module.exports = app;
+
+
+
+
+
+
+
+
+// app.get("/", function(req, res) {
+//     // If there's nothing in collection, scrape
+//     if (!MONGODB_URI.articles) {
+//         res.redirect("/scrape")
+//         return;
+//     // Else, show all
+//     } else {
+//         res.redirect("/all")
+//     }
+// });
+
+// // "Show All" route
+// app.get("/all", function(req, res) {
+//     Article.find({}).then(function(dbArticle) {
+//     let articles = [];
+//         for (const el of dbArticle) {
+//             articles.push({ id: el._id, title: el.title, desc: el.desc, url: el.url, image: el.image });
+//         };
+//         res.render("index", { article: articles });
+//     });
+// });
+
+// app.get("/notes", function(req, res) {
+//     Note.find({})
+//         .then(function(dbNote){
+//             res.json(dbNote);
+//         })
+//         .catch(function(err) {
+//             res.json(err);
+//         });
+// });
+
+//     // res.render 
+//     // note button
+//         // include art id as attr
+
+// // save notes /:id (post)
+//     // link note and article id
